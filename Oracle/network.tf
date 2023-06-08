f4cb04145ea0393a38434901f1936bdafbd26f21
Ahmed M. Atia <30297814+dreamwalker97@users.noreply.github.com>
2023-06-08 13:07:57 +0300

    network.tf

--- Oracle/network.tf
+++ Oracle/network.tf
@@ -75,38 +75,3 @@ resource "oci_core_subnet" "DBTier" {
   dns_label = "Private${var.dns_label}subnet1"
   route_table_id = oci_core_route_table.PrivateRT.id
 }
-
-/*
-resource "oci_core_security_list" "securitylist" {
-  display_name = "SL_public"
-  compartment_id = var.compartment_ocid
-  vcn_id = oci_core_virtual_network.vcn.id
-
-
-  egress_security_rules {
-    protocol = "all"
-    destination = "0.0.0.0/0"
-  }
-
-  ingress_security_rules {
-    protocol = "6"
-    source = "0.0.0.0/0"
-  }
-
-  tcp_options  {
-    min = 80 
-    max = 80 
-  }
-}
-
-ingress_security_rules {
-    protocol = "6"
-    source = "0.0.0.0/0"
-
-    tcp_options {
-        min = 22
-        max = 22
-    }
-
-}
-*/
